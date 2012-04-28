2fd95e49f2c8714a6af64dcd12c92c3401864067
Peter Kappus <peterk@peterkappus.com>
2012-04-28 22:47:59 +0100

    fix bad variable name

--- go.rb
+++ go.rb
@@ -20,7 +20,7 @@ queue_dir = run_dir + "/" + @config['queue_dir'] || "queue"
 def send_file(path,to,subject="Attachment: #{Time.now.strftime('%D %T')}",from=@from_address)  
   puts "Sending #{path} to #{@address}" if @debug
   #must pass "nil" argument for orion (OCS Solutions) to send properly
-  Pony.mail(:to => to, :from=>from, :subject => subject, :body => '', :via=>:sendmail, :attachments => {"image.jpg" => File.read(path)}, :via_options => { :location  => '/usr/sbin/sendmail', :arguments => nil})
+ # Pony.mail(:to => to, :from=>from, :subject => subject, :body => '', :via=>:sendmail, :attachments => {"image.jpg" => File.read(path)}, :via_options => { :location  => '/usr/sbin/sendmail', :arguments => nil})
 end
 
 
@@ -29,7 +29,7 @@ end
 #alert the admin if our queue is running low... need to DRY this up.
 if(Dir.glob("#{queue_dir}/#{glob}").count < 10)
   hostname = Socket.gethostbyname(Socket.gethostname).first
-  Pony.mail(:to => @notify_address, :from=>@from_address, :subject => "Your Ration queue is running low on #{hostname}!", :body => "Go fill \'er up... sent from #{$0}", :via=>:sendmail, :via_options => { :location  => '/usr/sbin/sendmail', :arguments => nil})
+  Pony.mail(:to => @notify_address, :from=>@from_address, :subject => "Your Ration queue is running low on #{hostname}!", :body => "Go fill \'er up... sent from #{run_dir}/#{$0}", :via=>:sendmail, :via_options => { :location  => '/usr/sbin/sendmail', :arguments => nil})
 end
 
 
